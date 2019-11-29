f2e1f5f6ec1cfbd2dcbf5d6cc30218a82df9d09c
Eric Nielsen <eric@amalgamar.com.br>
2019-11-28 21:52:42 -0500

    Scope local variables with anonymous function

--- init.zsh
+++ init.zsh
@@ -1,36 +1,34 @@
 #
-# sets up ssh-agent
+# Set up ssh-agent
 #
 
-# don't do anything unless we can actually use ssh-agent
-if (( ! ${+commands[ssh-agent]} )); then
-  return 1
-fi
-
-ssh-add -l &>/dev/null
-if (( ? == 2 )); then
-  # Unable to contact the authentication agent
-
-  # Load stored agent connection info
-  local ssh_env="${HOME}/.ssh-agent"
-  [[ -r ${ssh_env} ]] && source ${ssh_env} >/dev/null
-
+# Don't do anything unless we can actually use ssh-agent
+(( ${+commands[ssh-agent]} )) && () {
   ssh-add -l &>/dev/null
   if (( ? == 2 )); then
-      # Start agent and store agent connection info
-      (umask 066; ssh-agent >! ${ssh_env})
-      source ${ssh_env} >/dev/null
-  fi
-fi
+    # Unable to contact the authentication agent
 
-# Load identities
-ssh-add -l &>/dev/null
-if (( ? == 1 )); then
-  local -a zssh_ids
-  zstyle -a ':zim:ssh' ids 'zssh_ids'
-  if (( ${#zssh_ids} )); then
-    ssh-add "${HOME}/.ssh/${^zssh_ids[@]}" 2>/dev/null
-  else
-    ssh-add 2>/dev/null
+    # Load stored agent connection info
+    local -r ssh_env=${HOME}/.ssh-agent
+    [[ -r ${ssh_env} ]] && source ${ssh_env} >/dev/null
+
+    ssh-add -l &>/dev/null
+    if (( ? == 2 )); then
+        # Start agent and store agent connection info
+        (umask 066; ssh-agent >! ${ssh_env})
+        source ${ssh_env} >/dev/null
+    fi
   fi
-fi
+
+  # Load identities
+  ssh-add -l &>/dev/null
+  if (( ? == 1 )); then
+    local -a zssh_ids
+    zstyle -a ':zim:ssh' ids 'zssh_ids'
+    if (( ${#zssh_ids} )); then
+      ssh-add ${HOME}/.ssh/${^zssh_ids} 2>/dev/null
+    else
+      ssh-add 2>/dev/null
+    fi
+  fi
+}
